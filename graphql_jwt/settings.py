e87588383841bebda2f5e3719018d67ef589dce1
mongkok <domake.io@gmail.com>
2017-11-07 01:34:18 +0530

    Settings timedelta env

--- graphql_jwt/settings.py
+++ graphql_jwt/settings.py
@@ -1,20 +1,21 @@
-import datetime
 import environ
 
+from datetime import timedelta
 from django.conf import settings
 
-env = environ.Env()
+
+class Env(environ.Env):
+
+    def timedelta(self, var, default=environ.Env.NOTSET):
+
+        def cast(value):
+            args = [val.split('=') for val in value.split(',') if val]
+            return timedelta(**{name: float(val) for name, val in args})
+
+        return self.get_value(var, cast=cast, default=default)
 
 
-def timedelta(name, **defaults):
-    args = env.dict(name, default={})
-
-    if args:
-        args = {k: int(v) for k, v in args.items()}
-    else:
-        args = defaults
-    return datetime.timedelta(**args)
-
+env = Env()
 
 JWT_ALGORITHM = env('JWT_ALGORITHM', default='HS256')
 JWT_AUDIENCE = env('JWT_AUDIENCE', default=None)
@@ -25,11 +26,17 @@ JWT_SECRET_KEY = env('JWT_SECRET_KEY', default=settings.SECRET_KEY)
 
 JWT_VERIFY = env.bool('JWT_VERIFY', True)
 JWT_VERIFY_EXPIRATION = env.bool('JWT_VERIFY_EXPIRATION', False)
-JWT_EXPIRATION_DELTA = timedelta('JWT_EXPIRATION_DELTA', seconds=60 * 5)
+
+JWT_EXPIRATION_DELTA = env.timedelta(
+    'JWT_EXPIRATION_DELTA',
+    timedelta(seconds=60 * 5)
+)
 
 JWT_ALLOW_REFRESH = env.bool('JWT_ALLOW_REFRESH', True)
 JWT_VERIFY_REFRESH_EXPIRATION =\
     env.bool('JWT_VERIFY_REFRESH_EXPIRATION', True)
 
-JWT_REFRESH_EXPIRATION_DELTA =\
-    timedelta('JWT_REFRESH_EXPIRATION_DELTA', days=7)
+JWT_REFRESH_EXPIRATION_DELTA = env.timedelta(
+    'JWT_REFRESH_EXPIRATION_DELTA',
+    timedelta(days=7)
+)
