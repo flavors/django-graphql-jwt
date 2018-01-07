f77b6a89a6d45bddc89d643b239849205b66cb6a
mongkok <domake.io@gmail.com>
2018-01-08 01:26:59 +0530

    JWT_LEEWAY timedelta type

--- graphql_jwt/settings.py
+++ graphql_jwt/settings.py
@@ -22,7 +22,7 @@ JWT_ALGORITHM = env('JWT_ALGORITHM', default='HS256')
 JWT_AUDIENCE = env('JWT_AUDIENCE', default=None)
 JWT_AUTH_HEADER_PREFIX = env('JWT_AUTH_HEADER_PREFIX', default='JWT')
 JWT_ISSUER = env('JWT_ISSUER', default=None)
-JWT_LEEWAY = env.int('JWT_LEEWAY', 0)
+JWT_LEEWAY = env.timedelta('JWT_LEEWAY', 0)
 JWT_SECRET_KEY = env('JWT_SECRET_KEY', default=settings.SECRET_KEY)
 
 JWT_VERIFY = env.bool('JWT_VERIFY', True)
@@ -30,9 +30,11 @@ JWT_VERIFY_EXPIRATION = env.bool('JWT_VERIFY_EXPIRATION', False)
 
 JWT_EXPIRATION_DELTA = env.timedelta(
     'JWT_EXPIRATION_DELTA',
-    timedelta(seconds=60 * 5))
+    timedelta(seconds=60 * 5),
+)
 
 JWT_ALLOW_REFRESH = env.bool('JWT_ALLOW_REFRESH', True)
 JWT_REFRESH_EXPIRATION_DELTA = env.timedelta(
     'JWT_REFRESH_EXPIRATION_DELTA',
-    timedelta(days=7))
+    timedelta(days=7),
+)
