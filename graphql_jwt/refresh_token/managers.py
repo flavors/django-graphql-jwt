0ae8cc7d34cae47471270a8b74d65966cd56a0a2
mongkok <domake.io@gmail.com>
2018-10-17 11:44:47 +0700

    Improved expired annotation

--- graphql_jwt/refresh_token/managers.py
+++ graphql_jwt/refresh_token/managers.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models import Case, ExpressionWrapper, F
+from django.db.models import Case
 from django.db.models import Value as V
 from django.db.models import When
 from django.utils import timezone
@@ -10,13 +10,10 @@ from ..settings import jwt_settings
 class RefreshTokenQuerySet(models.QuerySet):
 
     def expired(self):
+        expires = timezone.now() - jwt_settings.JWT_REFRESH_EXPIRATION_DELTA
         return self.annotate(
-            expires=ExpressionWrapper(
-                F('created') + V(jwt_settings.JWT_REFRESH_EXPIRATION_DELTA),
-                output_field=models.DateTimeField(),
-            ),
             expired=Case(
-                When(expires__lt=timezone.now(), then=V(True)),
+                When(created__lt=expires, then=V(True)),
                 output_field=models.BooleanField(),
                 default=V(False),
             ),
