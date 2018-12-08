631c97c649a7d69623a5d2cfa66a939897b71f19
mongkok <domake.io@gmail.com>
2018-12-08 11:48:19 +0700

    Added test_get_token

--- tests/refresh_token/test_models.py
+++ tests/refresh_token/test_models.py
@@ -18,10 +18,21 @@ class AbstractRefreshTokenTests(UserTestCase):
 
     def test_generate_token(self):
         token = self.refresh_token.generate_token()
+        n_bytes = jwt_settings.JWT_REFRESH_TOKEN_N_BYTES
 
-        self.assertEqual(
-            len(token),
-            jwt_settings.JWT_REFRESH_TOKEN_N_BYTES * 2)
+        self.assertEqual(len(token), n_bytes * 2)
+
+    def test_get_token(self):
+        self.refresh_token.token = 'hashed'
+        token = self.refresh_token.get_token()
+
+        self.assertEqual(self.refresh_token._cached_token, token)
+        self.assertNotEqual(self.refresh_token.token, token)
+
+        del self.refresh_token._cached_token
+        token = self.refresh_token.get_token()
+
+        self.assertEqual(self.refresh_token.token, token)
 
     def test_is_expired(self):
         with refresh_expired():
