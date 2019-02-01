3c2a686270ac5d7b293bba5fa46886962848dd24
Arthur Holzner <arthur.holz.91@gmail.com>
2019-02-01 14:50:50 +0100

    added blacklist no to stdout

--- graphql_jwt/refresh_token/management/commands/buildblacklist.py
+++ graphql_jwt/refresh_token/management/commands/buildblacklist.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from django.template.defaultfilters import pluralize
 
 from graphql_jwt.refresh_token.blacklist import set_blacklist
 
@@ -9,5 +10,13 @@ class Command(BaseCommand):
     help = 'Build initial blacklist'
 
     def handle(self, *args, **options):
-        for refresh_token in get_refresh_token_model().objects.filter(revoked__isnull=False):
+        qs = get_refresh_token_model().objects.filter(revoked__isnull=False)
+        for refresh_token in qs.all():
             set_blacklist(refresh_token)
+
+        no = qs.count()
+        msg = 'Successfully blacklisted {} token{}'.format(
+            no,
+            pluralize(no))
+
+        self.stdout.write(self.style.SUCCESS(msg))
