e59fed56c2d7a3eccf3cae5498bc6636f27df38b
Arthur <arthur.holz.91@gmail.com>
2019-02-01 13:12:14 +0100

    removed uneeded param

--- graphql_jwt/refresh_token/management/commands/buildblacklist.py
+++ graphql_jwt/refresh_token/management/commands/buildblacklist.py
@@ -8,7 +8,7 @@ from ...utils import get_refresh_token_model
 class Command(BaseCommand):
     help = 'Build initial blacklist'
 
-    def handle(self, expired, *args, **options):
+    def handle(self, *args, **options):
         qs = get_refresh_token_model().objects.filter(revoked__isnull=False)
         for refresh_token in qs.all():
             set_blacklist(refresh_token)
