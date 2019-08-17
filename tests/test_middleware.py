0c87ded01d7a0c6d1f6abb219615f01ef1018141
mongkok <domake.io@gmail.com>
2019-08-17 22:24:50 +0400

    Removed DjangoMiddleware

--- tests/test_middleware.py
+++ tests/test_middleware.py
@@ -1,10 +1,4 @@
-import json
-import warnings
-
 from django.contrib.auth.models import AnonymousUser
-from django.http import JsonResponse
-
-from graphene_django.settings import graphene_settings
 
 from graphql_jwt.exceptions import JSONWebTokenError
 from graphql_jwt.middleware import JSONWebTokenMiddleware, allow_any
@@ -13,87 +7,6 @@ from graphql_jwt.settings import jwt_settings
 from .compat import mock
 from .decorators import override_jwt_settings
 from .testcases import TestCase
-
-
-class DjangoMiddlewareTests(TestCase):
-
-    def setUp(self):
-        super(DjangoMiddlewareTests, self).setUp()
-
-        self.get_response_mock = mock.Mock(return_value=JsonResponse({}))
-        self.middleware = JSONWebTokenMiddleware(self.get_response_mock)
-
-    def test_deprecation_warning(self):
-        graphene_settings.MIDDLEWARE = []
-
-        with warnings.catch_warnings(record=True) as warning_list:
-            JSONWebTokenMiddleware()
-            self.assertTrue(warning_list)
-
-        graphene_settings.MIDDLEWARE = [JSONWebTokenMiddleware]
-
-    def test_authenticate(self):
-        headers = {
-            jwt_settings.JWT_AUTH_HEADER_NAME: '{0} {1}'.format(
-                jwt_settings.JWT_AUTH_HEADER_PREFIX,
-                self.token),
-        }
-
-        request = self.request_factory.get('/', **headers)
-        self.middleware(request)
-
-        self.get_response_mock.assert_called_once_with(request)
-        self.assertEqual(request.user, self.user)
-
-    @mock.patch('graphql_jwt.middleware.authenticate', return_value=None)
-    def test_not_authenticate(self, authenticate_mock):
-        headers = {
-            jwt_settings.JWT_AUTH_HEADER_NAME: '{0} {1}'.format(
-                jwt_settings.JWT_AUTH_HEADER_PREFIX,
-                self.token),
-        }
-
-        request = self.request_factory.get('/', **headers)
-        self.middleware(request)
-
-        self.get_response_mock.assert_called_once_with(request)
-        authenticate_mock.assert_called_with(request=request)
-        self.assertFalse(hasattr(request, 'user'))
-
-    def test_invalid_token(self):
-        headers = {
-            jwt_settings.JWT_AUTH_HEADER_NAME: '{} invalid'.format(
-                jwt_settings.JWT_AUTH_HEADER_PREFIX),
-        }
-
-        request = self.request_factory.get('/', **headers)
-        response = self.middleware(request)
-        content = json.loads(response.content.decode('utf-8'))
-
-        self.assertIsNotNone(content['errors'])
-        self.get_response_mock.assert_not_called()
-
-    def test_header_not_found(self):
-        request = self.request_factory.get('/')
-        self.middleware(request)
-
-        self.get_response_mock.assert_called_once_with(request)
-        self.assertFalse(hasattr(request, 'user'))
-
-    @mock.patch('graphql_jwt.middleware.authenticate')
-    def test_already_authenticated(self, authenticate_mock):
-        headers = {
-            jwt_settings.JWT_AUTH_HEADER_NAME: '{0} {1}'.format(
-                jwt_settings.JWT_AUTH_HEADER_PREFIX,
-                self.token),
-        }
-
-        request = self.request_factory.get('/', **headers)
-        request.user = self.user
-        self.middleware(request)
-
-        self.get_response_mock.assert_called_once_with(request)
-        authenticate_mock.assert_not_called()
 
 
 class AuthenticateByHeaderTests(TestCase):
