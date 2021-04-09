5cfc896457c71061aaf58eef0d4cd5f429535349
mongkok <domake.io@gmail.com>
2021-04-09 12:29:11 +0700

    Removed signals providing_args

--- graphql_jwt/signals.py
+++ graphql_jwt/signals.py
@@ -1,4 +1,7 @@
 from django.dispatch import Signal
 
-token_issued = Signal(providing_args=['request', 'user'])
-token_refreshed = Signal(providing_args=['request', 'user'])
+# providing_args=['request', 'user']
+token_issued = Signal()
+
+# providing_args=['request', 'user']
+token_refreshed = Signal()
