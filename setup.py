f908c1b1ad1740b438cd13801193aec5f5ceec7e
mongkok <domake.io@gmail.com>
2021-04-09 10:42:20 +0700

    Added support for PyJWT2

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     install_requires=[
         'Django>=1.11',
         'graphene-django>=3.0.0b1',
-        'PyJWT>=1.5.0',
+        'PyJWT>=2,<3',
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
